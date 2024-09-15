7418c2459a26e0687d2e230e06680f37ae9cc88c
GaoNeng-wWw <gaonenggg@outlook.com>
2024-09-15 20:08:17 +0800

    nest.js

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,64 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
+import { I18nContext } from 'nestjs-i18n';
+import { I18nTranslations } from '../.generate/i18n.generated';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly reflector: Reflector,
+    private readonly authService: AuthService
+  ) {}
+  async canActivate(ctx: ExecutionContext): Promise<boolean> {
+    const i18n = I18nContext.current<I18nTranslations>();
+    const isPublic = this.reflector.getAllAndOverride('isPublic', [
+      ctx.getHandler(),
+      ctx.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    const req = ctx.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(req);
+    if (!token) {
+      throw new HttpException(
+        i18n.t('exception.common.tokenError', {
+          lang: I18nContext.current().lang,
+        }),
+        HttpStatus.FORBIDDEN
+      );
+    }
+    try {
+      const payload = await this.jwt.decode(token);
+      req['user'] = payload;
+      return this.authService.getToken(payload.email).then((redisToken) => {
+        // 如果Redis中没有token或者token不匹配，返回false
+        if (!redisToken || redisToken !== token) {
+          return false;
+        }
+        return true;
+      });
+    } catch (err) {
+      throw new HttpException(
+        i18n.t('exception.common.tokenExpire', {
+          lang: I18nContext.current().lang,
+        }),
+        HttpStatus.BAD_REQUEST
+      );
+    }
+  }
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
